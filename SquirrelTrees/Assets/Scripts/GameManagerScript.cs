e3408c10be34ce17a022a4fb5abd35d15d0928fb
reahaas <reahaas@gmail.com>
2023-10-23 01:24:11 +0300

    Add 3 player mode, and fix UI buttons size

--- SquirrelTrees/Assets/Scripts/GameManagerScript.cs
+++ SquirrelTrees/Assets/Scripts/GameManagerScript.cs
@@ -22,13 +22,20 @@ public class GameManagerScript : MonoBehaviour
     private TextMeshProUGUI mainMenuText;
     private Button startGameButton;
     private Button SetTwoPlayersModeButton;
+    private Button SetThreePlayersModeButton;    
     private Button SetFourPlayersModeButton;
     private static bool _isGameRunning = false;
     private static string winnerName = "";
 
+    // playerMode must be static to survive the scene reload on reset game!!
     private static int playerMode = 2;
     private List<GameObject> fourPlayersModeObjects = new List<GameObject>();
-
+    private List<GameObject> threePlayersModeObjects = new List<GameObject>();
+    private List<GameObject> threeRemovePlayersModeObjects = new List<GameObject>();
+    private List<GameObject> emptyList = new List<GameObject>();
+    private List<GameObject> ActivePlayersModeObjects;
+    private List<GameObject> NonActivePlayersModeObjects;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -43,13 +50,24 @@ public class GameManagerScript : MonoBehaviour
         this.SetTwoPlayersModeButton = mainMenuButtons[1];
         SetTwoPlayersModeButton.onClick.AddListener(setTwoPlayersMode);
 
-        this.SetFourPlayersModeButton = mainMenuButtons[2];
+        this.SetThreePlayersModeButton = mainMenuButtons[2];
+        SetThreePlayersModeButton.onClick.AddListener(setThreePlayersMode);
+
+        this.SetFourPlayersModeButton = mainMenuButtons[3];
         SetFourPlayersModeButton.onClick.AddListener(setFourPlayersMode);
 
         this.fourPlayersModeObjects.Add(player3);
         this.fourPlayersModeObjects.Add(player3Panel);
         this.fourPlayersModeObjects.Add(player4);
         this.fourPlayersModeObjects.Add(player4Panel);
+        
+        this.threePlayersModeObjects.Add(player3);
+        this.threePlayersModeObjects.Add(player3Panel);
+
+        
+        this.threeRemovePlayersModeObjects.Add(player4);
+        this.threeRemovePlayersModeObjects.Add(player4Panel);
+
         setPlayersMode();
     }
 
@@ -83,21 +101,42 @@ public class GameManagerScript : MonoBehaviour
         setPlayersMode();
     }
     
+    public void setThreePlayersMode(){
+        playerMode = 3;
+        setPlayersMode();
+    }
+
     public void setFourPlayersMode(){
         playerMode = 4;
         setPlayersMode();
     }
 
-    public void setPlayersMode(){  
+    public void setPlayersMode(){ 
+        // Check game mode
         if (playerMode == 2){
-            foreach(GameObject obj in this.fourPlayersModeObjects){
-                obj.SetActive(false);
-            }
+            this.NonActivePlayersModeObjects = this.fourPlayersModeObjects;
+            this.ActivePlayersModeObjects = this.emptyList;
+        }
+        else if (playerMode == 3){
+            this.ActivePlayersModeObjects = this.threePlayersModeObjects;
+            this.NonActivePlayersModeObjects = this.threeRemovePlayersModeObjects;
+        }
+        else if (playerMode == 4){
+            this.ActivePlayersModeObjects = this.fourPlayersModeObjects;
+            this.NonActivePlayersModeObjects = this.emptyList;
         }
         else{
-            foreach(GameObject obj in this.fourPlayersModeObjects){
-                obj.SetActive(true);
-            }
+            Debug.Log("playerMode is not valid: " + playerMode);
+        }
+
+        // Set active objects
+        foreach(GameObject obj in this.ActivePlayersModeObjects){
+            obj.SetActive(true);
+        }
+
+        // Set non active objects
+        foreach(GameObject obj in this.NonActivePlayersModeObjects){
+            obj.SetActive(false);
         }
     }
 
